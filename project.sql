90436170e1ff7a81a504d6a3b6fcf4cc10dd4c1c
afahimi <afahimi@students.cs.ubc.ca>
2023-11-30 08:09:19 -0800

    fixed sql insertion delay problem

--- project.sql
+++ project.sql
@@ -144,6 +144,7 @@ INSERT INTO Locations (LocationName, LocationType) VALUES ('The Great Forest', '
 INSERT INTO Locations (LocationName, LocationType) VALUES ('The End', 'Endgame');
 INSERT INTO Locations (LocationName, LocationType) VALUES ('Atlantis', 'Sea');
 INSERT INTO Locations (LocationName, LocationType) VALUES ('Verudon', 'City');
+COMMIT;
 
 INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('10-20-103', 'Floating City');
 INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('15-25-20', 'The Great Forest');
@@ -151,6 +152,7 @@ INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('5-15-20', '
 INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('5-15-22', 'The Great Forest');
 INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('20-30-40', 'The End');
 INSERT INTO CoordinateLocations (Coordinates, LocationName) VALUES ('22-36-40', 'The End');
+COMMIT;
 
 INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterName, Coordinates) VALUES (1382, 100, 28, 38, 'RemmyRoblox', '10-20-103');
 INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterName, Coordinates) VALUES (3312, 90, 4, 40, 'Mercury1989', '15-25-20');
@@ -162,65 +164,74 @@ INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterNam
 INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterName, Coordinates) VALUES (2003, 82, 3, 60, 'Bug', '5-15-22');
 INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterName, Coordinates) VALUES (2004, 70, 2, 70, 'Demon', '20-30-40');
 INSERT INTO CharacterInfo (CharacterID, Health, OverallLevel, Mana, CharacterName, Coordinates) VALUES (2005, 50, 1, 80, 'Demon Dark', '22-36-40');
+COMMIT;
 
 INSERT INTO Factions (FactionName, FactionType) VALUES ('Skyguard', 'Holy');
 INSERT INTO Factions (FactionName, FactionType) VALUES ('Forestwatch', 'Neutral');
 INSERT INTO Factions (FactionName, FactionType) VALUES ('Endkeepers', 'Dark');
 INSERT INTO Factions (FactionName, FactionType) VALUES ('Seaguard', 'Neutral');
 INSERT INTO Factions (FactionName, FactionType) VALUES ('Citywatch', 'Lawful');
+COMMIT;
 
 INSERT INTO MemberOf (CharacterID, FactionName) VALUES (1382, 'Skyguard');
 INSERT INTO MemberOf (CharacterID, FactionName) VALUES (3312, 'Forestwatch');
 INSERT INTO MemberOf (CharacterID, FactionName) VALUES (5869, 'Forestwatch');
 INSERT INTO MemberOf (CharacterID, FactionName) VALUES (1129, 'Endkeepers');
 INSERT INTO MemberOf (CharacterID, FactionName) VALUES (1269, 'Endkeepers');
+COMMIT;
 
 INSERT INTO Skill (SkillName, SkillType) VALUES ('Sky Slash', 'Attack');
 INSERT INTO Skill (SkillName, SkillType) VALUES ('Forest Heal', 'Heal');
 INSERT INTO Skill (SkillName, SkillType) VALUES ('End Shield', 'Defense');
 INSERT INTO Skill (SkillName, SkillType) VALUES ('Sea Wave', 'Attack');
 INSERT INTO Skill (SkillName, SkillType) VALUES ('City Guard', 'Defense');
+COMMIT;
 
 INSERT INTO Develops (SkillName, CharacterID, CurrentLevel) VALUES ('Sky Slash', 1382, 10);
 INSERT INTO Develops (SkillName, CharacterID, CurrentLevel) VALUES ('Forest Heal', 3312, 5);
 INSERT INTO Develops (SkillName, CharacterID, CurrentLevel) VALUES ('Forest Heal', 5869, 3);
 INSERT INTO Develops (SkillName, CharacterID, CurrentLevel) VALUES ('End Shield', 1129, 8);
 INSERT INTO Develops (SkillName, CharacterID, CurrentLevel) VALUES ('City Guard', 1269, 2);
+COMMIT;
 
 INSERT INTO Player (CharacterID, InventoryID) VALUES (1382, 1001);
 INSERT INTO Player (CharacterID, InventoryID) VALUES (3312, 1002);
 INSERT INTO Player (CharacterID, InventoryID) VALUES (5869, 1003);
 INSERT INTO Player (CharacterID, InventoryID) VALUES (1129, 1004);
 INSERT INTO Player (CharacterID, InventoryID) VALUES (1269, 1005);
+COMMIT;
 
 INSERT INTO NonPlayableCharacter (CharacterID, Personality) VALUES (2001, 'Aggressive');
 INSERT INTO NonPlayableCharacter (CharacterID, Personality) VALUES (2002, 'Helpful');
 INSERT INTO NonPlayableCharacter (CharacterID, Personality) VALUES (2003, 'Mysterious');
 INSERT INTO NonPlayableCharacter (CharacterID, Personality) VALUES (2004, 'Talkative');
 INSERT INTO NonPlayableCharacter (CharacterID, Personality) VALUES (2005, 'Silent');
+COMMIT;
 
 INSERT INTO Inventory (InventoryID, InventorySize) VALUES (1001, 20);
 INSERT INTO Inventory (InventoryID, InventorySize) VALUES (1002, 15);
 INSERT INTO Inventory (InventoryID, InventorySize) VALUES (1003, 18);
 INSERT INTO Inventory (InventoryID, InventorySize) VALUES (1004, 25);
 INSERT INTO Inventory (InventoryID, InventorySize) VALUES (1005, 10);
+COMMIT;
 
 INSERT INTO Item (ItemID, ItemName) VALUES (1001, 'Sword of Light');
 INSERT INTO Item (ItemID, ItemName) VALUES (1002, 'Dark Shield');
 INSERT INTO Item (ItemID, ItemName) VALUES (1003, 'Health Potion');
 INSERT INTO Item (ItemID, ItemName) VALUES (1004, 'Bow of Eternity');
 INSERT INTO Item (ItemID, ItemName) VALUES (1005, 'Ring of Strength');
+COMMIT;
 
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1001, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1002, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1004, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1003, 1);
-INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1007, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1001, 1005, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1002, 1002, 1);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1003, 1003, 5);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1004, 1004, 2);
 INSERT INTO Contains (InventoryID, ItemID, InventoryQuantity) VALUES (1005, 1005, 3);
+COMMIT;
 
 INSERT INTO Interactions (PlayerCharacterID, NonPlayableCharacterCharacterID, InteractionTime, InteractionType) 
 VALUES (1382, 2001, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634280600, 'SECOND'), 'Conversation');
@@ -232,6 +243,7 @@ INSERT INTO Interactions (PlayerCharacterID, NonPlayableCharacterCharacterID, In
 VALUES (1129, 2004, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634538600, 'SECOND'), 'Trade');
 INSERT INTO Interactions (PlayerCharacterID, NonPlayableCharacterCharacterID, InteractionTime, InteractionType) 
 VALUES (1269, 2005, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634624700, 'SECOND'), 'Conversation');
+COMMIT;
 
 INSERT INTO YieldsQuest(QuestID, PlayerCharacterID, NonPlayableCharacterID, InteractionTime, QuestLevel) 
 VALUES (101, 1382, 2001, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634280600, 'SECOND'), 68);
@@ -243,16 +255,19 @@ INSERT INTO YieldsQuest(QuestID, PlayerCharacterID, NonPlayableCharacterID, Inte
 VALUES (12, 1129, 2004, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634538600, 'SECOND'), 120);
 INSERT INTO YieldsQuest(QuestID, PlayerCharacterID, NonPlayableCharacterID, InteractionTime, QuestLevel) 
 VALUES (99, 1269, 2005, TIMESTAMP '1970-01-01 00:00:00' + NUMTODSINTERVAL(1634624700, 'SECOND'), 38);
+COMMIT;
 
 INSERT INTO RewardItems (RewardQuantity, ItemID) VALUES (1, 1001);
 INSERT INTO RewardItems (RewardQuantity, ItemID) VALUES (2, 1002);
 INSERT INTO RewardItems (RewardQuantity, ItemID) VALUES (5, 1003);
 INSERT INTO RewardItems (RewardQuantity, ItemID) VALUES (3, 1004);
 INSERT INTO RewardItems (RewardQuantity, ItemID) VALUES (4, 1005);
+COMMIT;
 
 INSERT INTO QuestRewards (QuestID, RewardQuantity) VALUES (101, 1);
 INSERT INTO QuestRewards (QuestID, RewardQuantity) VALUES (112, 1);
 INSERT INTO QuestRewards (QuestID, RewardQuantity) VALUES (312, 5);
 INSERT INTO QuestRewards (QuestID, RewardQuantity) VALUES (12, 1);
 INSERT INTO QuestRewards (QuestID, RewardQuantity) VALUES (99, 3);
+COMMIT;
 
